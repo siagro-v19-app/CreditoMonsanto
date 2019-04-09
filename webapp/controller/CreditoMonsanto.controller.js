211bc180cfe159633bc71a5678d4aa941a7f1c6d
matusca96 <gomesloud@gmail.com>
2019-04-09 16:01:09 -0300

    Cadastro de crédito monsanto.

---
+++ webapp/controller/CreditoMonsanto.controller.js
@@ -0,0 +1,159 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox", 
+	"br/com/idxtecCotacaoMoeda/services/Session"
+], function(Controller, MessageBox, Session) {
+	"use strict";
+
+	return Controller.extend("br.com.idxtecCreditoMonsanto.controller.CreditoMonsanto", {
+		onInit: function(){
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		},
+		
+		onRefresh: function(){
+			var oModel = this.getOwnerComponent().getModel();
+			oModel.refresh(true);
+			this.getView().byId("tableCredito").clearSelection();
+		},
+		
+		onIncluir: function(){
+			var oDialog = this._criarDialog();
+			var oModel = this.getOwnerComponent().getModel();
+			var oViewModel = this.getModel("view");
+			
+			oViewModel.setData({
+				titulo: "Inserir Crédito Monsanto",
+				msg: "Crédito Monsanto inserido com sucesso!"
+			});
+			
+			oDialog.unbindElement();
+			oDialog.setEscapeHandler(function(oPromise){
+				if(oModel.hasPendingChanges()){
+					oModel.resetChanges();
+				}
+			});
+			
+			var oContext = oModel.createEntry("/CreditoMonsantos",{
+				properties: {
+					"Id": 0,
+					"Codigo": "",
+					"Descricao": "",
+					"Empresa" : Session.get("EMPRESA_ID"),
+					"Usuario": Session.get("USUARIO_ID"),
+					"EmpresaDetails": { __metadata: { uri: "/Empresas(" + Session.get("EMPRESA_ID") + ")"}},
+					"UsuarioDetails": { __metadata: { uri: "/Usuarios(" + Session.get("USUARIO_ID") + ")"}}
+				}
+			});
+			
+			oDialog.setBindingContext(oContext);
+			oDialog.open();
+		},
+		
+		onEditar: function(){
+			var oDialog = this._criarDialog();
+			var oTable = this.byId("tableCredito");
+			var nIndex = oTable.getSelectedIndex();
+			var oViewModel = this.getModel("view");
+			
+			oViewModel.setData({
+				titulo: "Editar Crédito Monsanto",
+				msg: "Crédito Monsanto alterado com sucesso!"
+			});
+			
+			if(nIndex === -1){
+				MessageBox.warning("Selecione um crédito monsanto da tabela!");
+				return;
+			}
+			
+			var oContext = oTable.getContextByIndex(nIndex);
+			
+			oDialog.bindElement(oContext.sPath);
+			oDialog.open();
+		},
+		
+		onRemover: function(){
+			var that = this;
+			var oTable = this.byId("tableCredito");
+			var nIndex = oTable.getSelectedIndex();
+			
+			if(nIndex === -1){
+				MessageBox.warning("Selecione um crédito monsanto da tabela!");
+				return;
+			}
+			
+			MessageBox.confirm("Deseja remover esse crédito monsanto?", {
+				onClose: function(sResposta){
+					if(sResposta === "OK"){
+						MessageBox.success("Crédito Monsanto removido com sucesso!");
+						that._remover(oTable, nIndex);
+					} 
+				}      
+			});
+		},
+		
+		_remover: function(oTable, nIndex){
+			var oModel = this.getOwnerComponent().getModel();
+			var oContext = oTable.getContextByIndex(nIndex);
+			
+			oModel.remove(oContext.sPath,{
+				success: function(){
+					oModel.refresh(true);
+					oTable.clearSelection();
+				}
+			});
+		},
+		
+		_criarDialog: function(){
+			var oView = this.getView();
+			var oDialog = this.byId("CreditoMonsantoDialog");
+			
+			if(!oDialog){
+				oDialog = sap.ui.xmlfragment(oView.getId(), "br.com.idxtecCreditoMonsanto.view.CreditoMonsantoDialog", this);
+				oView.addDependent(oDialog);
+			}
+			
+			return oDialog;
+		},
+		
+		onSaveDialog: function(){
+			var oView = this.getView();
+			var oModel = this.getOwnerComponent().getModel();
+			var oViewModel = this.getModel("view");
+			
+			if(this._checarCampos(this.getView()) === true){
+				MessageBox.warning("Preencha todos os campos obrigatórios!");
+				return;
+			} else{
+				oModel.submitChanges({
+					success: function(){
+						oModel.refresh(true);
+						MessageBox.success(oViewModel.getData().msg);
+						oView.byId("CreditoMonsantoDialog").close();
+						oView.byId("tableCredito").clearSelection();                         
+					}
+				});
+			}
+		},
+		
+		onCloseDialog: function(){
+			var oModel = this.getOwnerComponent().getModel();
+			
+			if(oModel.hasPendingChanges()){
+				oModel.resetChanges();
+			} 
+			this.byId("CreditoMonsantoDialog").close();
+		},
+		
+		_checarCampos: function(oView){
+			if(oView.byId("codigo").getValue() === "" || oView.byId("descricao").getValue() === ""){
+				return true;
+			} else{
+				return false; 
+			}
+		},
+		
+		getModel: function(sModel){
+			return this.getOwnerComponent().getModel(sModel);
+		}
+	});
+});
